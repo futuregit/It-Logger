6e61fde979a6bd8ca1e642418eade47645490b58
futuregit <gmail.com>
2021-12-13 04:42:23 -0500

    Connected a few components to the store

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,25 +1,37 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR} from '../actions/types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "../actions/types";
 
 const initialState = {
-    logs: null,
-    current: null,
-    loading: false,
-    error: null
-}
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case SET_LOADING:
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false,
+      };
+    case ADD_LOG:
         return {
             ...state,
-            loading: true
-        },
-    case LOGS_ERROR:
-        console.error("This is the error: ", action.payload)
-        return {
-            ...state,
-            error: action.payload
+            logs: [...state.logs, action.payload],
+            loading: false
         }
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGS_ERROR:
+      console.error("This is the error: ", action.payload);
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
