50633e45f1adcfdf8fa260d2c728962669d39a71
futuregit <gmail.com>
2021-12-13 19:51:19 -0500

    Implemented add tech action

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -8,14 +8,47 @@ import {
 
 // Get techs from server
 export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
+
+// Add technician to server
+export const addTech = (tech) => async (dispatch) => {
     try {
       setLoading();
   
-      const res = await fetch("/techs");
+      const res = await fetch("/techs", {
+          method: 'POST',
+          body: JSON.stringify(tech),
+          headers: {
+              'Content-Type': "application/json"
+          }
+      });
       const data = await res.json();
   
       dispatch({
-        type: GET_TECHS,
+        type: ADD_TECH,
         payload: data,
       });
     } catch (err) {
@@ -24,11 +57,4 @@ export const getTechs = () => async (dispatch) => {
         payload: err.response.statusText,
       });
     }
-  };
-
-// Set loading to true
-export const setLoading = () => {
-    return {
-      type: SET_LOADING,
-    };
   };
